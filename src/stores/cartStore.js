79413ecec29cd9430552d70290194ee5bd0b953d
Artur Kucinski <artuyees@gmail.com>
2022-01-25 00:47:34 +0100

    Cart now stays in local storage.

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,3 +1,5 @@
+import { browser } from '$app/env';
 import { writable } from 'svelte/store';
 
-export let cartItems = writable([]);
+export let cartItems = writable(browser && (JSON.parse(localStorage.getItem('cartItems')) || ''));
+cartItems.subscribe((val) => browser && localStorage.setItem('cartItems', JSON.stringify(val)));
