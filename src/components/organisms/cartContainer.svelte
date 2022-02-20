3f34922b6e92372e0c5ba7da31d9c59fc394d55a
Artur Kucinski <artuyees@gmail.com>
2022-02-20 22:10:16 +0100

    updated cart

--- src/components/organisms/cartContainer.svelte
+++ src/components/organisms/cartContainer.svelte
@@ -9,22 +9,18 @@
 	.box {
 		display: flex;
 		flex-direction: column;
-		background-color: #f0f0f0;
 		width: fit-content;
 		max-width: 1000px;
 		min-width: fit-content;
 		margin: 50px auto;
 		border-radius: 10px;
 		padding: 10px;
-		box-shadow: 0px 0px 27px -3px rgba(92, 91, 91, 1);
 	}
 	@media (max-width: 768px) {
 		.box {
 			width: 100%;
 			margin: none;
 			box-shadow: none;
-			border-top: 1px solid gray;
-			border-bottom: 1px solid gray;
 			border-radius: 0;
 			padding: 10px 0px;
 		}
